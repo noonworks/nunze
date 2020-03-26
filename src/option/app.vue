c7e1bbec17539b2e42768b3949209fc97964b489
noonworks <noonworks@gmail.com>
2020-03-26 09:05:23 +0900

    Add root HTML.

---
+++ src/option/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data: () => {
+    return {};
+  },
+});
+</script>
+
+<style></style>
---
+++ src/option/main.ts
@@ -0,0 +1,6 @@
+import App from './app.vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new App();
+  app.$mount('#app');
+});
