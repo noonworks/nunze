5391dcaf18df8948c11b186fce289f352d93695a
noonworks <noonworks@gmail.com>
2020-04-08 17:56:35 +0900

    Add minimized version of vue-ctk-date-time-picker.

---
+++ lib/vue-ctk-date-time-picker/dist/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'vue-ctk-date-time-picker' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const VueCtkDateTimePicker: any;
+  export default VueCtkDateTimePicker;
+}
